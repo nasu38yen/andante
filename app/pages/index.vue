46d9ba41ef1d3cef39488e787914bfe39048737a
島　浩文 <shima@strawhat.onmicrosoft.com>
2025-05-30 13:58:02 +0900

    preview deploy test

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,7 +13,7 @@
   <main>
     <PageTitle>ようこそ Andante伝言板へ</PageTitle>
     <div class="flex flex-col justify-center mb-8">
-      <p class="text-lg mb-8">ログインしてお進みください</p>
+      <p class="text-lg mb-8">ログインしてお進みくださいな</p>
       <div class="flex space-x-4">
         <UButton to="/login" class="">ログイン</UButton>
       </div>        
