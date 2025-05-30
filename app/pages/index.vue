a8dd4f01f7bda33231a51b1e090c85b204b1f29a
島　浩文 <shima@strawhat.onmicrosoft.com>
2025-05-30 13:33:13 +0900

    Preview準備

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
   const { loggedIn, user } = useUserSession();
-  onMounted(() => {
+  onBeforeMount(() => {
     if (loggedIn.value) {      
       navigateTo('/board')
     } else {
-      console.log('user is null');
+      //console.log('user is null');
     }
   });  
 </script>
 
 <template>
   <main>
-    <PageTitle>ようこそ Andante伝言板</PageTitle>
+    <PageTitle>ようこそ Andante伝言板へ</PageTitle>
     <div class="flex flex-col justify-center mb-8">
       <p class="text-lg mb-8">ログインしてお進みください</p>
       <div class="flex space-x-4">
